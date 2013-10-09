46bd1054093c455e86afcaed9e26e583ae802072
peng <wanpeng@mantekiiMac.local>
2013-10-09 23:47:19 +0800

    make attributes

--- resource/script/ah/unit/IPObject.js
+++ resource/script/ah/unit/IPObject.js
@@ -1,9 +1,31 @@
-define(['dojo/_base/declare','dijit/_WidgetBase','dijit/_TemplatedMixin','dojo/_base/lang'],
-	function(declare,_WidgetBase,_TemplateMixin,lang){
+define(['dojo/_base/declare','dijit/_WidgetBase','dijit/_TemplatedMixin','dojo/on','dojo/_base/lang'],
+	function(declare,_WidgetBase,_TemplateMixin,on,lang){
+
 		return declare('ah/unit/IPObject',[_WidgetBase,_TemplateMixin],{
+
 			templateString : '<div class="">'+
 							'<p><input type="text" class="" data-dojo-attach-point="ipEl" /></p>'+
 							'<div data-dojo-attach-point="ipList"></div>'+
-							'</div>'
+							'</div>',
+			name : '',
+
+			postCreate : function(){
+				console.log('postCreate');	
+				this.inherited(arguments);
+
+				this.own(
+					on(this.ipEl,'keyup',lang.hitch(this,this._handleKeyup))	
+				);
+			},
+
+			_handleKeyup : function(e){
+				this.set('name','hello');
+				console.log(this.get('name'));
+			},
+
+			_setNameAttr : function(){
+				console.log('set name');
+			}
+
 		});
 });
