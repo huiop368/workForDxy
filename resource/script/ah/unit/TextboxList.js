a1a79bf93baa2cc5bb60d05475e12e262521e7e5
pwan <pwan@pwantekiMacBook-Pro.local>
2013-10-23 17:45:29 +0800

    add Textboxlist for test

---
+++ resource/script/ah/unit/TextboxList.js
@@ -0,0 +1,6 @@
+define(['dojo/_base/declare','dijit/_WidgetBase','dijit/_TemplatedMixin','dojo/on','dojo/_base/lang'],
+	function(declare,_WidgetBase,_TemplateMixin,on,lang){
+		return declare('ah/unit/TextboxList',[_WidgetBase,_TemplateMixin],{
+			
+		});
+});
